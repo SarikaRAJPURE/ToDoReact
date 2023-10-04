f31920435d854c9e2e94b59dbddebff07c1564bd
SarikaRAJPURE <sprajpure@gmail.com>
2023-10-04 13:42:41 -0400

    Refractor code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,50 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ToDoItem from "./components/ToDoItem";
+import InputArea from "./components/InputArea";
 //import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
-  const [inputText, setInputText] = useState("");
+  
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
+  
 
-  function addItem() {
+  function addItem(inputText) {
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
-    setInputText("");
+    
   }
 
   function deleteItem(id) {
     setItems((prevItems) => {
-      return prevItems.filter((item,index) => {
+      return prevItems.filter((item, index) => {
         return index !== id;
       });
     });
   }
-  
+
   return (
     <div className="App">
-           <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
+      <div className="container">
+        <div className="heading">
+          <h1>To-Do List</h1>
+        </div>
+       <InputArea onAdd={addItem}/>
+        <div>
+          <ul>
+            {items.map((todoItem, index) => (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onChecked={deleteItem}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {items.map((todoItem,index) => (
-            <ToDoItem
-              key={index}
-              id={index}
-              text={todoItem}
-              onChecked={deleteItem}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
     </div>
   );
 }
